c6b59aa27678fc3342e33632cfb6ba113fc776d0
Techrankbook <techrankbook@gmail.com>
2024-11-28 15:00:20 +0530

    create pages and router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-
-
+import React from "react";
+import { BrowserRouter as Router,Routes,Route } from "react-router";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import Offers from "./pages/Offers";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import ForgotPassword from "./pages/ForgotPassword";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hello world</h1>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/offers" element={<Offers />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+      </Routes>
+    </Router>
   );
 }
 
